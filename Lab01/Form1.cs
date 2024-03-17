33682c85eb9a1ed68d8ad3f669a6734efa9c2f93
cmendoza-dev <carlos.mendoza.s@edu.pe>
2024-03-17 12:28:44 -0500

    Agregar archivos de proyecto.

---
+++ Lab01/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Form1 : Form
+
+    {
+        int nc = 0;
+
+        void generarNumero()
+        {
+            nc++;
+            //txtNumGuia.text = nc.ToString("0000");
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            generarNumero();
+        }
+
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Instanciando la clase RegistrarTransporte
+            RegistrarTransporte rt = new RegistrarTransporte();
+            rt.NomCliente = txtNomCliente.Text;
+            rt.Descrip = txtDescrip.Text;
+            //rt.TnMetricas = txtTnM.Text;
+
+
+
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab01/RegistrarTransporte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class RegistrarTransporte
+    {
+        public DateTime FecDoc { get; set; }
+        public string NomCliente { get; set; }
+        public string Descrip { get; set; }
+        public double TnMetricas { get; set; }
+
+
+        //Método de descuento
+        public double CostoTransporte()
+        {
+            return TnMetricas * 150;
+        }
+
+        public double Seguro()
+        {
+            return CostoTransporte() * 0.02;
+        }
+
+        public double Monto()
+        {
+            return CostoTransporte() + Seguro();
+        }
+
+        public double IGV()
+        {
+            return Monto() * 0.18;
+        }
+
+        public double ImporteTotal()
+        {
+            return Monto() + IGV();
+        }
+
+
+
+
+    }
+}
